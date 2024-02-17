6815a496443c8a8d0c3cc45178e19f2800c6439f
unknown <duje.miljevic@gmail.com>
2024-02-17 18:13:25 +0100

    renamed frontend project

--- CanDatabaseManagementSystem.UI/Pages/Error.cshtml.cs
+++ CanDatabaseManagementSystem.UI/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace CanDatabaseManagementSystem.BlazorServer.Pages
+namespace CanDatabaseManagementSystem.UI.Pages
 {
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	[IgnoreAntiforgeryToken]

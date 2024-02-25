02ed285c9eb93802a1d6bde81951bb151b09a005
unknown <duje.miljevic@gmail.com>
2024-02-25 18:01:53 +0100

    added migration for updated models

--- CanDatabaseManagementSystem.DAL/Migrations/20240220210738_addedDbcFileTable.Designer.cs
+++ CanDatabaseManagementSystem.DAL/Migrations/20240220210738_addedDbcFileTable.Designer.cs
@@ -3,6 +3,7 @@ using CanDatabaseManagementSystem.DAL.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CanDatabaseManagementSystem.DAL.Migrations
 {
     [DbContext(typeof(CanDatabaseContext))]
-    partial class CanDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240220210738_addedDbcFileTable")]
+    partial class addedDbcFileTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CanDatabaseManagementSystem.DAL/Migrations/20240220210738_addedDbcFileTable.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CanDatabaseManagementSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedDbcFileTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MessageId",
+                table: "Signals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DbcFileId",
+                table: "NetworkNodes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DbcFileId",
+                table: "Messages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "DbcFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DbcFiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Signals_MessageId",
+                table: "Signals",
+                column: "MessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NetworkNodes_DbcFileId",
+                table: "NetworkNodes",
+                column: "DbcFileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_DbcFileId",
+                table: "Messages",
+                column: "DbcFileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_DbcFiles_DbcFileId",
+                table: "Messages",
+                column: "DbcFileId",
+                principalTable: "DbcFiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NetworkNodes_DbcFiles_DbcFileId",
+                table: "NetworkNodes",
+                column: "DbcFileId",
+                principalTable: "DbcFiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Signals_Messages_MessageId",
+                table: "Signals",
+                column: "MessageId",
+                principalTable: "Messages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_DbcFiles_DbcFileId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_NetworkNodes_DbcFiles_DbcFileId",
+                table: "NetworkNodes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Signals_Messages_MessageId",
+                table: "Signals");
+
+            migrationBuilder.DropTable(
+                name: "DbcFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Signals_MessageId",
+                table: "Signals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NetworkNodes_DbcFileId",
+                table: "NetworkNodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_DbcFileId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "MessageId",
+                table: "Signals");
+
+            migrationBuilder.DropColumn(
+                name: "DbcFileId",
+                table: "NetworkNodes");
+
+            migrationBuilder.DropColumn(
+                name: "DbcFileId",
+                table: "Messages");
+        }
+    }
+}
